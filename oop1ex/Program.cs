6c914c5f32fe237de0e60fa786c8bdec69e643e1
tonyk <tonyk@LAPTOP-V0FCFK8L>
2022-11-28 09:50:18 +0000

    Add project files.

---
+++ oop1ex/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace oop1ex
+{
+    internal class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            //Accepting user input
+            Console.WriteLine("Enter your first name:");
+            //setting it in variable Firstname
+            string firstName = Console.ReadLine();
+
+            
+            Console.WriteLine("Enter your last name:");
+            string lastName = Console.ReadLine(); //set() is called to assign value
+
+            
+            Console.WriteLine("Please enter the date you were born in the format DD/MM/YYYYY: ");
+           
+            DateTime dateOfBirth = DateTime.Parse(Console.ReadLine());
+           
+            //creating an object p of Person class passing it arguments
+            Person p = new Person(firstName, lastName, dateOfBirth);
+             
+            Console.Clear();
+            
+
+            //accessing the getters in the properties
+            //and printing them using object p
+            Console.WriteLine($"\nYour full name is {p.FirstName} {p.LastName} and you were born on a {p.DateOfBirth.DayOfWeek.ToString()} .");
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ oop1ex/person.cs
@@ -0,0 +1,51 @@
+﻿namespace oop1ex
+{
+    public class Person
+    {
+        //variables/fields
+        private string firstName;
+        private string lastName;
+        private DateTime dateOfBirth;
+        
+
+        
+        //Constructor
+        //called to set field values of each new object when instantiated in Main method
+        public Person(string firstName, string lastName, DateTime dateOfBirth )
+        {
+            
+           
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            
+        }
+
+
+        //properties
+        //get accessor = used to return the property value
+        //set accessor = used to assign a new value
+        //value keyword = defines the value being assigned by the set (parameter) 
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+  
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+            
+        }
+        public DateTime DateOfBirth
+        {
+             get { return dateOfBirth; }
+             set { dateOfBirth = value; }
+            
+        }
+    }
+}
+
+
